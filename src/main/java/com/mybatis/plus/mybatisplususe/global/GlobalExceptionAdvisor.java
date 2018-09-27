117d34618c9b1dfbf8b2e8bb7a68daeea464436d
zhuquanwen <5211314a>
2018-09-27 15:34:19 +0800

    验证码功能

--- src/main/java/com/mybatis/plus/mybatisplususe/global/GlobalExceptionAdvisor.java
+++ src/main/java/com/mybatis/plus/mybatisplususe/global/GlobalExceptionAdvisor.java
@@ -1,5 +1,9 @@
 package com.mybatis.plus.mybatisplususe.global;
 
+import com.baomidou.kaptcha.exception.KaptchaException;
+import com.baomidou.kaptcha.exception.KaptchaIncorrectException;
+import com.baomidou.kaptcha.exception.KaptchaNotFoundException;
+import com.baomidou.kaptcha.exception.KaptchaTimeoutException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,6 +20,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  */
 @RestControllerAdvice
 public class GlobalExceptionAdvisor {
+
+    @ExceptionHandler(value = KaptchaException.class)
+    public String kaptchaExceptionHandler(KaptchaException kaptchaException) {
+        if (kaptchaException instanceof KaptchaIncorrectException) {
+            return "验证码不正确";
+        } else if (kaptchaException instanceof KaptchaNotFoundException) {
+            return "验证码未找到";
+        } else if (kaptchaException instanceof KaptchaTimeoutException) {
+            return "验证码过期";
+        } else {
+            return "验证码渲染失败";
+        }
+    }
+
     @ExceptionHandler(value = Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String test(Exception e){
